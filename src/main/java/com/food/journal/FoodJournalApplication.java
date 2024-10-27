5214d213721f3846971f810cf187091a4d00fc3d
Jan Jankowski <j.jankowski91@gmail.com>
2024-10-27 12:20:14 +0100

    init project

---
+++ src/main/java/com/food/journal/FoodJournalApplication.java
@@ -0,0 +1,13 @@
+package com.food.journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodJournalApplication.class, args);
+	}
+
+}
